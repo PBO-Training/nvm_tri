8a06f333b4a875b98842d5e1bd7f17eb8cdcc3ac
tri72027 <tri72027@gmail.com>
2021-02-01 22:47:31 +0700

    c

--- BE/application/src/main/java/com/travel/application/travelservice/trip/service/TripProcess.java
+++ BE/application/src/main/java/com/travel/application/travelservice/trip/service/TripProcess.java
@@ -2,7 +2,6 @@ package com.travel.application.travelservice.trip.service;
 
 import java.sql.Date;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import javax.persistence.EntityManager;
--- FE/coach/src/app/app-routing.module.ts
+++ FE/coach/src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ChooseTripComponent } from './choose-trip/choose-trip.component';
 import { InfoCustomerComponent } from './info-customer/info-customer.component';
+import { InfoTicketComponent } from './info-ticket/info-ticket.component';
+import { PaidSuccessComponent } from './paid-success/paid-success.component';
 import { SearchRouteComponent } from './search-route/search-route.component';
 
 
@@ -17,6 +19,14 @@ const routes: Routes = [
   {
     path: "infocustomer",
     component: InfoCustomerComponent
+  },
+  {
+    path: "infoticket",
+    component: InfoTicketComponent
+  },
+  {
+    path: "paidsuccess",
+    component: PaidSuccessComponent
   }
 ];
 
